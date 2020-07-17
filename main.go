7468b5df3d7fa212893d5c136829d97d5174924c
Preston Kemp <preston@prestonkemp.com>
2020-07-16 23:41:53 -0500

    changes to signing

--- main.go
+++ main.go
@@ -3,11 +3,10 @@ package main
 import (
 	"crypto/rsa"
 	"crypto/x509"
-	"encoding/json"
 	"encoding/pem"
 	"errors"
+	"fmt"
 	"github.com/aws/aws-sdk-go/service/cloudfront/sign"
-	"html/template"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -19,37 +18,80 @@ type VideoPageData struct {
 	PosterURL string
 }
 
-func main() {
+func handler(w http.ResponseWriter, r *http.Request) {
 	keyID := "APKAIJ6FUGQSVTWYI4QQ"
-	coverURL := "https://cdn.honeybadgers.tech/video/Honeybadger-Dark.jpg"
 	fileKey, err := ioutil.ReadFile("priv.key")
 	if err != nil {
 		log.Fatal(err)
 	}
 	rawKey := string(fileKey)
+	fmt.Fprintf(w, "Hello World!")
+	privKey, _ := ParseRsaPrivateKeyFromPemStr(rawKey)
+	s := sign.NewCookieSigner(keyID, privKey)
+	//// Get Signed cookies for a resource that will expire in 1 hour
+	//cookies, err := s.Sign("*", time.Now().Add(1 * time.Hour))
+	//if err != nil {
+	//	fmt.Println("failed to create signed cookies", err)
+	//	return
+	//}
 
-	tmpl := template.Must(template.ParseFiles("video.html"))
-	http.HandleFunc("/watch", func(w http.ResponseWriter, r *http.Request) {
-		video := r.URL.Query().Get("video")
-		rawURL := "https://cdn.honeybadgers.tech/video/" + video
-		data := generateVideoPageData(keyID, rawKey, rawURL, coverURL)
-		tmpl.Execute(w, data)
+	// Or get Signed cookies for a resource that will expire in 1 hour
+	// and set path and domain of cookies
+	cookies, err := s.Sign("*", time.Now().Add(1 * time.Hour), func(o *sign.CookieOptions) {
+		o.Path = "/"
+		o.Domain = "video.honeybadgers.tech"
 	})
-	http.HandleFunc("/getVideo", func(w http.ResponseWriter, r *http.Request) {
-		video := r.URL.Query().Get("video")
-		rawURL := "https://cdn.honeybadgers.tech/video/" + video
-		data := generateVideoPageData(keyID, rawKey, rawURL, coverURL)
+	if err != nil {
+		fmt.Println("failed to create signed cookies", err)
+		return
+	}
 
-		js, err := json.Marshal(data)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
+	// Server Response via http.ResponseWriter
+	for _, c := range cookies {
+		http.SetCookie(w, c)
+	}
+	http.FileServer(http.Dir("/")).ServeHTTP(w, r)
+}
 
-		w.Header().Set("Content-Type", "application/json")
-		w.Write(js)
-	})
+func main() {
 
+
+	//
+	//
+	//tmpl := template.Must(template.ParseFiles("video.html"))
+	//http.HandleFunc("/watch", func(w http.ResponseWriter, r *http.Request) {
+	//	video := r.URL.Query().Get("video")
+	//	rawURL := "https://cdn.honeybadgers.tech/video/" + video
+	//	data := generateVideoPageData(keyID, rawKey, rawURL, coverURL)
+	//	tmpl.Execute(w, data)
+	//})
+	//http.HandleFunc("/getVideo", func(w http.ResponseWriter, r *http.Request) {
+	//	video := r.URL.Query().Get("video")
+	//	rawURL := "https://cdn.honeybadgers.tech/video/" + video
+	//	data := generateVideoPageData(keyID, rawKey, rawURL, coverURL)
+	//
+	//	js, err := json.Marshal(data)
+	//	if err != nil {
+	//		http.Error(w, err.Error(), http.StatusInternalServerError)
+	//		return
+	//	}
+	//
+	//	w.Header().Set("Content-Type", "application/json")
+	//	w.Write(js)
+	//})
+
+
+
+	//// Client request via the cookie jar
+	//if client.CookieJar != nil {
+	//	for _, c := range cookies {
+	//		client.Cookie(w, c)
+	//	}
+	//}
+
+
+	http.HandleFunc("/", handler)
+	
 	http.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("./assets"))))
 	http.Handle("/dist/", http.StripPrefix("/dist/", http.FileServer(http.Dir("./dist"))))
 	http.ListenAndServe(":8000", nil)
